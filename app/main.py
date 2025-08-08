9cb9496f2ce32c9e76ad37666e90304b475ca3f2
Serhii Chumak <s4umak@gmail.com>
2025-08-08 16:56:50 +0300

    Solution py-distance-class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,46 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    @staticmethod
+    def _to_km(other: float) -> float:
+        if isinstance(other, Distance):
+            return other.km
+        return other
+
+    def __add__(self, other: float) -> "Distance":
+        km_to_add = self._to_km(other)
+        return Distance(self.km + km_to_add)
+
+    def __iadd__(self, other: float) -> "Distance":
+        km_to_add = self._to_km(other)
+        self.km += km_to_add
+        return self
+
+    def __mul__(self, other: float) -> "Distance":
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: float) -> "Distance":
+        result_km = round(self.km / other, 2)
+        return Distance(result_km)
+
+    def __lt__(self, other: float) -> float:
+        return self.km < self._to_km(other)
+
+    def __gt__(self, other: float) -> float:
+        return self.km > self._to_km(other)
+
+    def __eq__(self, other: float) -> float:
+        return self.km == self._to_km(other)
+
+    def __le__(self, other: float) -> float:
+        return self.km <= self._to_km(other)
+
+    def __ge__(self, other: float) -> float:
+        return self.km >= self._to_km(other)
